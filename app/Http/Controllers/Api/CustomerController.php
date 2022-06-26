4fb5ee6efb2559652fb5feef204baa8de500695d
Ruzaik <mnmruzaik@gmail.com>
2022-06-26 23:08:59 +0530

    customer crud operations

---
+++ app/Http/Controllers/Api/CustomerController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Customer\CustomerRequest;
+use App\Http\Resources\CustomerResource;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       return CustomerResource::collection(Customer::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CustomerRequest $request)
+    {
+        
+        $customer = Customer::create($request->validated());
+
+        return new CustomerResource($customer);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Customer $customer)
+    {
+        return new CustomerResource($customer);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CustomerRequest $request, Customer $customer)
+    {
+        $customer->update($request->validated());
+
+        return new CustomerResource($customer);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Customer $customer)
+    {
+        $customer->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Customer Deleted Successfully'
+        ]);
+    }
+}
